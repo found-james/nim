e556900fc1f67368b038ffd4657a72db502f6580
James Suescun <james.steven2318@gmail.com>
2022-01-25 20:33:19 -0500

    used js to create the wireframe for nim game

--- script.js
+++ script.js
@@ -56,50 +56,88 @@
 // }
 // //next move has to be the bots
 
-//list of ids for rows 
-const idsForRows = ["first-row", "second-row", "third-row", "forth-row", "fifth-row", "sixth-row", "seventh-row"];
-//const center = document.querySelector("#center");
 
+const bodyOfHtml = document.querySelector("body");
+bodyOfHtml.style.height = "100vh";
+bodyOfHtml.style.width = "100%";
+
+const mainConatiner = document.createElement("main");
+mainConatiner.style.width = "80%";
+mainConatiner.style.height = "40%";
+mainConatiner.style.display = "flex";
+bodyOfHtml.appendChild(mainConatiner);
+
+const leftSideMain = document.createElement("article");
+leftSideMain.id = "left-side";
+leftSideMain.classList.add("edge");
+mainConatiner.appendChild(leftSideMain);
+
+
+const centerMain = document.createElement("article");
+centerMain.id = "center";
+mainConatiner.appendChild(centerMain);
+
+const rightSideMain = document.createElement("article");
+rightSideMain.id = "right-side";
+rightSideMain.classList.add("edge");
+mainConatiner.appendChild(rightSideMain);
+
+const buttonsForUser = document.createElement("div");
+buttonsForUser.id = "player-buttons"
+bodyOfHtml.appendChild(buttonsForUser);
+
+const buttonOne = document.createElement("button");
+buttonOne.id = "one";
+buttonOne.style.width = "25%";
+buttonOne.style.height = "15%";
+buttonsForUser.appendChild(buttonOne);
+
+const buttonTwo = document.createElement("button");
+buttonTwo.style.width = "25%";
+buttonTwo.style.height = "15%";
+buttonTwo.id = "two";
+buttonsForUser.appendChild(buttonTwo);
+
+const buttonThree = document.createElement("button");
+buttonThree.style.width = "25%";
+buttonThree.style.height = "15%";
+buttonThree.id = "three";
+buttonsForUser.appendChild(buttonThree);
+
+const idsForRows = ["first-row", "second-row", "third-row", "forth-row", "fifth-row", "sixth-row", "seventh-row"]; 
  
+const createRows = (arr) => {
 
-// const createRows = () => {
-//     const row = document.createElement("div");
-//     row.classList.add("row");
-//     row.id = idsForRows[i];
-//     center.appendChild(row);
-// }
+    for (let id of idsForRows){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.id = id;
+        centerMain.appendChild(row);
+    }
 
-class RowContainer {
-    constructor(id){
-        this.id = id;
 
+
+    // for (let i = 0; i < arr.length; i++){
+    //     const row = document.createElement("div");
+    //     row.classList.add("row");
+    //     row.id = idsForRows[i];
+    //     center.appendChild(row);
+    // }
+}
+
+createRows(idsForRows);
+
+const createSticks = (num) => {
+    
+    for (let i = 0; i < num; i++){
+    const stick = document.createElement("span");
+    stick.classList.add("stick");
     }
 
     
-
-//This code makes an array of future divs with their corresponding ids
-//I want this instance to become a div and I want to append it to <main>
-//Concerned that if I use a loop to create instances all instances will have the same variable name
-//I have everything in the array so I could the array index to refer to specific instances
-
-    static makeRowContainers (arr) {
-        const arrOfRows =[];
-        for (let i = 0; i < arr.length; i++){
-
-            arrOfRows.push(new RowContainer(arr[i]));
-
-    }
-    return arrOfRows;
-    }
-
 }
 
-
-const arrOfRows = RowContainer.makeRowContainers(idsForRows);
-console.log(arrOfRows);
-
-
-// createRows ();
+console.log(createSticks(17));
 
 // const createSticks = () => {
 //     const stick = document.createElement("span");
@@ -125,4 +163,24 @@ console.log(arrOfRows);
     
 //     }
 
+// }
+
+
+// class RowContainer {
+//     constructor(id){
+//         this.id = id;
+
+//     }
+// //This code makes an array of future divs with their corresponding ids
+// //I want this instance to become a div and I want to append it to <main>
+// //Concerned that if I use a loop to create instances all instances will have the same variable name
+// //I have everything in the array so I could the array index to refer to specific instances
+
+//     static makeRowContainers (arr) {
+//         const arrOfRows =[];
+//         for (let i = 0; i < arr.length; i++){
+//             arrOfRows.push(new RowContainer(arr[i]));
+//     }
+//     return arrOfRows;
+//     }
 // }
